1a26ab15dabc5f64a20500d6cebbb3ead2555954
milkasaf <103029257+milkasaf@users.noreply.github.com>
2022-06-19 16:00:15 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,222 @@
+function onEntry(entry) {
+   entry.forEach(change => {
+      if (change.isIntersecting) {
+
+   change.target.classList.add('element-show');      
+      }
+   });
+}
+
+let options = {
+   threshold: [0.5] };
+let observer = new 
+IntersectionObserver(onEntry, options);
+let elements = 
+document.querySelectorAll('.robot__img');
+
+for (const elm of elements) {
+      observer.observe(elm);
+}
+
+// 
+function onEntry(entry) {
+   entry.forEach(change => {
+      if (change.isIntersecting) {
+
+   change.target.classList.add('element-show');      
+      }
+   });
+}
+
+let optionss = {
+   threshold: [0.5] };
+let observerr = new 
+IntersectionObserver(onEntry, optionss);
+let elementss = 
+document.querySelectorAll('.robot__text');
+
+for (const elm of elementss) {
+      observerr.observe(elm);
+}
+
+// 
+
+function onEntry(entry) {
+   entry.forEach(change => {
+      if (change.isIntersecting) {
+
+   change.target.classList.add('element-show');      
+      }
+   });
+}
+
+let options3 = {
+   threshold: [0.5] };
+let observer3 = new 
+IntersectionObserver(onEntry, options3);
+let elements3 = 
+document.querySelectorAll('.cards__item1');
+
+for (const elm of elements3) {
+      observer3.observe(elm);
+}
+// 
+function onEntry(entry) {
+   entry.forEach(change => {
+      if (change.isIntersecting) {
+
+   change.target.classList.add('element-show');      
+      }
+   });
+}
+
+let options4 = {
+   threshold: [0.5] };
+let observer4 = new 
+IntersectionObserver(onEntry, options4);
+let elements4 = 
+document.querySelectorAll('.cards__item2');
+
+for (const elm of elements4) {
+      observer4.observe(elm);
+}
+// 
+function onEntry(entry) {
+   entry.forEach(change => {
+      if (change.isIntersecting) {
+
+   change.target.classList.add('element-show');      
+      }
+   });
+}
+
+let options5 = {
+   threshold: [0.5] };
+let observer5 = new 
+IntersectionObserver(onEntry, options5);
+let elements5 = 
+document.querySelectorAll('.cards__item3');
+
+for (const elm of elements5) {
+      observer5.observe(elm);
+}
+// 
+function onEntry(entry) {
+   entry.forEach(change => {
+      if (change.isIntersecting) {
+
+   change.target.classList.add('element-show');      
+      }
+   });
+}
+
+let options6 = {
+   threshold: [0.5] };
+let observer6 = new 
+IntersectionObserver(onEntry, options6);
+let elements6 = 
+document.querySelectorAll('.cards__item4');
+
+for (const elm of elements6) {
+      observer6.observe(elm);
+}
+// 
+function onEntry(entry) {
+   entry.forEach(change => {
+      if (change.isIntersecting) {
+
+   change.target.classList.add('element-show');      
+      }
+   });
+}
+
+let options7 = {
+   threshold: [0.5] };
+let observer7 = new 
+IntersectionObserver(onEntry, options7);
+let elements7 = 
+document.querySelectorAll('.cards__item4');
+
+for (const elm of elements7) {
+      observer7.observe(elm);
+}
+// 
+function onEntry(entry) {
+   entry.forEach(change => {
+      if (change.isIntersecting) {
+
+   change.target.classList.add('element-show');      
+      }
+   });
+}
+
+let options8 = {
+   threshold: [0.5] };
+let observer8 = new 
+IntersectionObserver(onEntry, options8);
+let elements8 = 
+document.querySelectorAll('.cards__item5');
+
+for (const elm of elements8) {
+      observer8.observe(elm);
+}
+// 
+function onEntry(entry) {
+   entry.forEach(change => {
+      if (change.isIntersecting) {
+
+   change.target.classList.add('element-show');      
+      }
+   });
+}
+
+let options9 = {
+   threshold: [0.5] };
+let observer9 = new 
+IntersectionObserver(onEntry, options9);
+let elements9 = 
+document.querySelectorAll('.streamer__content');
+
+for (const elm of elements9) {
+      observer9.observe(elm);
+}
+
+function onEntry(entry) {
+   entry.forEach(change => {
+      if (change.isIntersecting) {
+
+   change.target.classList.add('element-show');      
+      }
+   });
+}
+
+let options10 = {
+   threshold: [0.5] };
+let observer10 = new 
+IntersectionObserver(onEntry, options10);
+let elements10 = 
+document.querySelectorAll('.reviews__text-descr');
+
+for (const elm of elements10) {
+      observer10.observe(elm);
+} 
+
+function onEntry(entry) {
+   entry.forEach(change => {
+      if (change.isIntersecting) {
+
+   change.target.classList.add('element-show');      
+      }
+   });
+}
+
+let options12 = {
+   threshold: [0.5] };
+let observer12 = new 
+IntersectionObserver(onEntry, options12);
+let elements12 = 
+document.querySelectorAll('.reviews__text-img');
+
+for (const elm of elements12) {
+      observer10.observe(elm);
+}
